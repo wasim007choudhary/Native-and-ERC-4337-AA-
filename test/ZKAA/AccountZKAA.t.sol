4c02f10e854989df33d995e708ff1d31b6185cef
wasim007choudhary <lesnarwasimff5@gmail.com>
2026-02-25 01:02:20 +0530

    done

--- test/ZKAA/AccountZKAA.t.sol
+++ test/ZKAA/AccountZKAA.t.sol
@@ -14,6 +14,10 @@ import {BOOTLOADER_FORMAL_ADDRESS} from "lib/foundry-era-contracts/src/system-co
 import {
     ACCOUNT_VALIDATION_SUCCESS_MAGIC
 } from "lib/foundry-era-contracts/src/system-contracts/contracts/interfaces/IAccount.sol";
+
+import {
+    SystemContractsCaller
+} from "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/SystemContractsCaller.sol";
 import {MessageHashUtils} from "lib/openzeppelin-contracts/contracts/utils/cryptography/MessageHashUtils.sol";
 
 contract AccountZKAATest is Test {
